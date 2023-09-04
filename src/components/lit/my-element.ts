2f90d1c60237f53978fe31187187927fe1788b18
ligdy <dy179912099@163.com>
2023-09-04 13:02:32 +0800

    feat: init🎉 lit add blog

--- src/components/lit/my-element.ts
+++ src/components/lit/my-element.ts
@@ -33,25 +33,30 @@ export class MyElement extends LitElement {
       <ul>
         <li>
           implement a mini
-          <a href="https://github.com/ligdy7/swr-min">swr-min</a>.
+          <a href="https://github.com/ligdy7/swr-min" target="_blank">swr-min</a
+          >.
         </li>
         <li>
-          some of <a href="https://github.com/ligdy7/ho-hooks">react hooks</a>.
+          some of
+          <a href="https://github.com/ligdy7/ho-hooks" target="_blank"
+            >react hooks</a
+          >.
         </li>
         <li>
           an expandable
-          <a href="https://github.com/ligdy7/ho-hooks"
+          <a href="https://github.com/ligdy7/ho-hooks" target="_blank"
             >Chrome bookmarks Quick Search</a
           >.
         </li>
         <li>
           A cube rotation touch program
-          <a href="https://github.com/ligdy7/uni-3d-translate "
+          <a href="https://github.com/ligdy7/uni-3d-translate " target="_blank"
             >uni-3d-translate
           </a>
         </li>
         <li>
-          a ssr website <a href="https://github.com/ligdy7/qwikbg ">qwikbg </a>
+          a ssr website
+          <a href="https://github.com/ligdy7/qwikbg" target="_blank">qwikbg </a>
         </li>
         <li>...</li>
       </ul>
