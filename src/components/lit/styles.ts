1b51ac8d67b8e13dd4b1ac5c26e4dacc92b37155
ligdy <dy179912099@163.com>
2023-09-04 17:22:09 +0800

    feat: init🎉

---
+++ src/components/lit/styles.ts
@@ -0,0 +1,24 @@
+import { css } from "lit";
+
+export const styles = [
+  css`
+    h1 {
+      font-size: 2rem;
+    }
+    p {
+      margin-bottom: 1em;
+    }
+    a {
+      color: var(--accent);
+    }
+    a:hover {
+      color: var(--accent);
+    }
+    p {
+      font-size: 1.2rem;
+    }
+    ul {
+      font-size: 1.1rem;
+    }
+  `,
+];
